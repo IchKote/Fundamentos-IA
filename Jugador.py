8d8bedda207d60ad08679a292b00834a8e89b61b
IchKote <167239730+IchKote@users.noreply.github.com>
2026-08-23 19:36:44 -0400

    Refactor Jugador class methods and naming conventions

--- Jugador.py
+++ Jugador.py
@@ -1,21 +1,30 @@
-from Reglas import Reglas 
+from Reglas import Reglas
 
-class Jugadores:
+
+class Jugador:
+    """Representa a un jugador de la partida."""
+
     def __init__(self, color):
         self.color = color
 
-    def ponerFicha(self, tablero, fila, columna):
+    def poner_ficha(self, tablero, fila, columna):
+        """Intenta colocar una ficha y voltea las capturadas."""
         if 0 < fila <= tablero.dimension and 0 < columna <= tablero.dimension:
-            if Reglas.espacioVacio(tablero, fila-1, columna-1):
-                if Reglas.jugadaValida(tablero, fila-1, columna-1, self.color) == False:
-                    tablero.grilla[fila-1][columna-1] = "-"
-                    print("Jugada inválida.")
+            if Reglas.espacio_vacio(tablero, fila - 1, columna - 1):
+                es_valida = Reglas.jugada_valida(
+                    tablero, fila - 1, columna - 1, self.color
+                )
+                if not es_valida:
+                    tablero.grilla[fila - 1][columna - 1] = "-"
+                    print("Jugada inválida. No captura fichas del rival.")
                     return False
                 else:
-                    tablero.grilla[fila-1][columna-1] = self.color
-                    Reglas.voltearFichas(tablero, fila-1, columna-1, self.color)
+                    tablero.grilla[fila - 1][columna - 1] = self.color
+                    Reglas.voltear_fichas(
+                        tablero, fila - 1, columna - 1, self.color
+                    )
                     return True
-            elif Reglas.espacioOcupado(tablero, fila-1, columna-1) == True:
+            elif Reglas.espacio_ocupado(tablero, fila - 1, columna - 1):
                 print("La celda ya está ocupada.")
                 return False
         else:

160bedc1f5603ad1f1dc4720e4e8432fc3a11657
IchKote <j.guzmanilabel@uandresbello.edu>
2026-09-03 23:21:47 -0400

    Agente.py alfabeta

--- Agente.py
+++ Agente.py
@@ -28,3 +28,17 @@ class Agente:
                         if alfa >= beta:
                             break
         return mejorPuntaje
+
+    def mejor_jugada(self, tablero):
+        mejorPuntaje = -inf
+        movimiento = None
+        for i in range(tablero.dimension):
+            for j in range(tablero.dimension):
+                if Reglas.jugada_valida(tablero, i, j, "B"):
+                    tablero[i][j] = "B"
+                    puntaje = self.alfabeta(tablero, False, -inf, inf)
+                    tablero[i][j] = "-"
+                    if puntaje > mejorPuntaje:
+                        mejorPuntaje = puntaje
+                        movimiento = (i, j)
+        return movimiento

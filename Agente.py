272b7d81ed3322e76558cd428b67d21b3b0dbce8
IchKote <167239730+IchKote@users.noreply.github.com>
2026-09-04 00:35:19 -0400

    Agente alfa-beta

--- Agente.py
+++ Agente.py
@@ -3,42 +3,55 @@ from Reglas import Reglas
 
 class Agente:
     def alfabeta(self, tablero, turnoAgente, alfa, beta):
+        """Calcula el valor minimax usando poda alfa-beta."""
         if turnoAgente:
             mejorPuntaje = -inf
             for i in range(tablero.dimension):
                 for j in range(tablero.dimension):
-                    if Reglas.jugada_valida(tablero, i, j, "B"):
-                        tablero[i][j] = "B"
+                    if (tablero.grilla[i][j] in ["-", "X"] and
+                            Reglas.jugada_valida(tablero, i, j, "B")):
+                        tablero.grilla[i][j] = "B"
+                        Reglas.voltear_fichas(tablero, i, j, "B")
                         puntaje = self.alfabeta(tablero, False, alfa, beta)
-                        tablero[i][j] = "-"
-                        mejorPuntaje = max(mejorPuntaje, puntaje)
+                        tablero.grilla[i][j] = "-"
+                        if puntaje > mejorPuntaje:
+                            mejorPuntaje = puntaje
                         alfa = max(alfa, mejorPuntaje)
-                        if alfa >= beta:
+                        if beta <= alfa:
                             break
         else:
             mejorPuntaje = inf
             for i in range(tablero.dimension):
                 for j in range(tablero.dimension):
-                    if Reglas.jugada_valida(tablero, i, j, "N"):
-                        tablero[i][j] = "N"
+                    if (tablero.grilla[i][j] in ["-", "X"] and
+                            Reglas.jugada_valida(tablero, i, j, "N")):
+                        tablero.grilla[i][j] = "N"
+                        Reglas.voltear_fichas(tablero, i, j, "N")
                         puntaje = self.alfabeta(tablero, True, alfa, beta)
-                        tablero[i][j] = "-"
-                        mejorPuntaje = min(mejorPuntaje, puntaje)
+                        tablero.grilla[i][j] = "-"
+                        if puntaje < mejorPuntaje:
+                            mejorPuntaje = puntaje
                         beta = min(beta, mejorPuntaje)
-                        if alfa >= beta:
+                        if beta <= alfa:
                             break
         return mejorPuntaje
 
     def mejor_jugada(self, tablero):
-        mejorPuntaje = -inf
+        """Retorna la mejor jugada para las fichas blancas."""
+        mejor_puntaje = -inf
         movimiento = None
+
         for i in range(tablero.dimension):
             for j in range(tablero.dimension):
-                if Reglas.jugada_valida(tablero, i, j, "B"):
-                    tablero[i][j] = "B"
+                if (tablero.grilla[i][j] in ["-", "X"] and
+                        Reglas.jugada_valida(tablero, i, j, "B")):
+                    tablero.grilla[i][j] = "B"
+                    Reglas.voltear_fichas(tablero, i, j, "B")
                     puntaje = self.alfabeta(tablero, False, -inf, inf)
-                    tablero[i][j] = "-"
-                    if puntaje > mejorPuntaje:
-                        mejorPuntaje = puntaje
+                    tablero.grilla[i][j] = "-"
+
+                    if puntaje > mejor_puntaje:
+                        mejor_puntaje = puntaje
                         movimiento = (i, j)
+
         return movimiento
